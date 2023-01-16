6599ed76a49800067488c19b9dc56d3a6547b986
Peter D'hondt <dhondt.peter@gmail.com>
2023-01-16 12:08:15 +0100

    added AbstractRepository

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -1,17 +1,8 @@
 package be.vdab;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Main {
-    private static final String URL = "jdbc:mysql://localhost/tuincentrum";
-    private static final String USER = "cursist";
-    private static final String PASSWORD = "cursist";
+
     public static void main(String[] args) {
-        try (var connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
-            System.out.println("Connectie geopend");
-    } catch (SQLException ex) {
-            ex.printStackTrace(System.err);
-        }
+
     }
 }
