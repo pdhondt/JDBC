d9f8165f0536294ca87fd1859e83c096017db182
Peter D'hondt <dhondt.peter@gmail.com>
2023-01-16 11:20:35 +0100

    update user

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -5,8 +5,8 @@ import java.sql.SQLException;
 
 public class Main {
     private static final String URL = "jdbc:mysql://localhost/tuincentrum";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Java@Vdab2023";
+    private static final String USER = "cursist";
+    private static final String PASSWORD = "cursist";
     public static void main(String[] args) {
         try (var connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
             System.out.println("Connectie geopend");
