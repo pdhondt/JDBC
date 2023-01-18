c425ab52d70042dd40231f276869394da378936c
Peter D'hondt <dhondt.peter@gmail.com>
2023-01-18 22:09:42 +0100

    14.2 Betere oplossing

--- src/main/java/be/vdab/repositories/PlantRepository.java
+++ src/main/java/be/vdab/repositories/PlantRepository.java
@@ -17,10 +17,11 @@ public class PlantRepository extends AbstractRepository {
                 set prijs = prijs * 1.1
                 """;
         try (var connection = super.getConnection();
-        var statement = connection.prepareStatement(sql)) {
+             var statement = connection.prepareStatement(sql)) {
             return statement.executeUpdate();
         }
     }
+
     public int verhoogPrijzenMet10ProcentByNaam(String naam) throws SQLException {
         var sql = """
                 update planten
@@ -28,22 +29,24 @@ public class PlantRepository extends AbstractRepository {
                 where naam = ?
                 """;
         try (var connection = super.getConnection();
-            var statement = connection.prepareStatement(sql)) {
+             var statement = connection.prepareStatement(sql)) {
             statement.setString(1, naam);
             return statement.executeUpdate();
         }
     }
+
     public List<String> findNamenByWoord(String woord) throws SQLException {
         var namen = new ArrayList<String>();
         try (var connection = super.getConnection();
-            var statement = connection.prepareCall("{call PlantNamenMetEenWoord(?)}")) {
+             var statement = connection.prepareCall("{call PlantNamenMetEenWoord(?)}")) {
             statement.setString(1, '%' + woord + '%');
-            for (var result = statement.executeQuery(); result.next();) {
+            for (var result = statement.executeQuery(); result.next(); ) {
                 namen.add(result.getString("naam"));
             }
             return namen;
         }
     }
+
     public void verhoogPrijzenBovenEnOnder100€() throws SQLException {
         var sqlVanaf100 = """
                 update planten
@@ -56,49 +59,49 @@ public class PlantRepository extends AbstractRepository {
                 where prijs < 100
                 """;
         try (var connection = super.getConnection();
-            var statementVanaf100 = connection.prepareStatement(sqlVanaf100);
-            var statementTot100 = connection.prepareStatement(sqlTot100)) {
+             var statementVanaf100 = connection.prepareStatement(sqlVanaf100);
+             var statementTot100 = connection.prepareStatement(sqlTot100)) {
             connection.setAutoCommit(false);
             statementVanaf100.executeUpdate();
             statementTot100.executeUpdate();
             connection.commit();
         }
     }
+
     public void verlaagPrijs(long id, BigDecimal nieuwePrijs) throws SQLException {
-        var sqlSelect = """
-                select prijs
-                from planten
-                where id = ?
-                for update
+        var sqlUpdate = """
+                update planten
+                set prijs = ?
+                where id = ? and ? > prijs / 2
                 """;
         try (var connection = super.getConnection();
-            var statementSelect = connection.prepareStatement(sqlSelect)) {
-            statementSelect.setLong(1, id);
+             var statementUpdate = connection.prepareStatement(sqlUpdate)) {
             connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
             connection.setAutoCommit(false);
-            var result = statementSelect.executeQuery();
-            if (result.next()) {
-                var oudePrijs = result.getBigDecimal("prijs");
-                var minimumNieuwePrijs = oudePrijs.divide(BigDecimal.valueOf(2), 2, RoundingMode.HALF_UP);
-                if (nieuwePrijs.compareTo(minimumNieuwePrijs) >= 0) {
-                    var sqlUpdate = """
-                            update planten
-                            set prijs = ?
-                            where id = ?
-                            """;
-                    try (var statementUpdate = connection.prepareStatement(sqlUpdate)) {
-                        statementUpdate.setBigDecimal(1, nieuwePrijs);
-                        statementUpdate.setLong(2, id);
-                        statementUpdate.executeUpdate();
-                        connection.commit();
-                        return;
-                    }
+            statementUpdate.setBigDecimal(1, nieuwePrijs);
+            statementUpdate.setLong(2, id);
+            statementUpdate.setBigDecimal(3, nieuwePrijs);
+            var aantalAangepast = statementUpdate.executeUpdate();
+            if (aantalAangepast == 1) {
+                connection.commit();
+                return;
+            }
+            var sqlSelect = """
+                    select count(*) as aantal
+                    from planten
+                    where id = ?
+                    """;
+            try (var statementSelect = connection.prepareStatement(sqlSelect)) {
+                statementSelect.setLong(1, id);
+                var result = statementSelect.executeQuery();
+                result.next();
+                if (result.getLong("aantal") == 0) {
+                    connection.rollback();
+                    throw new PlantNietGevondenException();
                 }
                 connection.rollback();
                 throw new PrijsTeLaagException();
             }
-            connection.rollback();
-            throw new PlantNietGevondenException();
         }
     }
 }
